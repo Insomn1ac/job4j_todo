775c12a26f1afa899638424475ce2c64e074e728
Insomn1ac <emelyanoff94@gmail.com>
2022-07-15 11:31:47 +0300

    Лямбды и шаблон wrapper. [#49295]

--- src/main/java/ru/job4j/todo/persistence/AccountDbStore.java
+++ src/main/java/ru/job4j/todo/persistence/AccountDbStore.java
@@ -1,17 +1,15 @@
 package ru.job4j.todo.persistence;
 
 import org.hibernate.HibernateException;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.Account;
 
-import javax.persistence.NoResultException;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class AccountDbStore {
+public class AccountDbStore implements IStore {
     private final SessionFactory sf;
 
     public AccountDbStore(SessionFactory sf) {
@@ -19,50 +17,29 @@ public class AccountDbStore {
     }
 
     public Optional<Account> add(Account account) {
-        try (Session session = sf.openSession()) {
-            session.beginTransaction();
-            session.save(account);
-            session.getTransaction().commit();
-            return Optional.of(account);
+        try {
+            return Optional.of((Account) tx(session -> session.save(account), sf));
         } catch (HibernateException e) {
             return Optional.empty();
         }
     }
 
     public Optional<Account> findAccountByLoginAndPassword(String login, String password) {
-        try (Session session = sf.openSession()) {
-            session.beginTransaction();
-            Optional<Account> account = session.createQuery("from Account where login = :fLogin and password = :fPass")
-                            .setParameter("fLogin", login)
-                            .setParameter("fPass", password)
-                            .uniqueResultOptional();
-            session.getTransaction().commit();
-            return account;
-        } catch (NoResultException e) {
-            return Optional.empty();
-        }
+        return tx(session -> session.createQuery("from Account where login = :fLogin and password = :fPass")
+                        .setParameter("fLogin", login)
+                        .setParameter("fPass", password)
+                        .uniqueResultOptional(),
+                sf);
     }
 
     public Optional<Account> findAccountByName(String name) {
-        try (Session session = sf.openSession()) {
-            session.beginTransaction();
-            Optional<Account> account = session.createQuery("from Account where name = :fName")
-                    .setParameter("fName", name)
-                    .uniqueResultOptional();
-            session.getTransaction().commit();
-            return account;
-        } catch (NoResultException e) {
-            return Optional.empty();
-        }
+        return tx(session -> session.createQuery("from Account where name = :fName")
+                        .setParameter("fName", name)
+                        .uniqueResultOptional(),
+                sf);
     }
 
     public List<Account> findAll() {
-        List<Account> accounts;
-        try (Session session = sf.openSession()) {
-            session.beginTransaction();
-            accounts = session.createQuery("from Account ").getResultList();
-            session.getTransaction().commit();
-        }
-        return accounts;
+        return tx(session -> session.createQuery("from Account ").getResultList(), sf);
     }
 }
--- src/main/java/ru/job4j/todo/persistence/ItemDbStore.java
+++ src/main/java/ru/job4j/todo/persistence/ItemDbStore.java
@@ -1,6 +1,5 @@
 package ru.job4j.todo.persistence;
 
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.Item;
@@ -8,10 +7,9 @@ import ru.job4j.todo.model.Item;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
-public class ItemDbStore {
+public class ItemDbStore implements IStore {
     private final SessionFactory sf;
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
@@ -20,91 +18,55 @@ public class ItemDbStore {
     }
 
     public Item add(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.save(item);
-        session.getTransaction().commit();
-        session.close();
-        return item;
+        return (Item) tx(session -> session.save(item),
+                sf);
     }
 
     public boolean update(Item item, int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        int itemUpdated = session.createQuery("update Item i "
-                        + "set i.created = :created, i.name = :name, i.description = :description, i.done = :done "
-                        + "where i.id = :fId")
-                .setParameter("created", LocalDateTime.now().format(formatter))
-                .setParameter("name", item.getName())
-                .setParameter("description", item.getDescription())
-                .setParameter("done", item.isDone())
-                .setParameter("fId", id)
-                .executeUpdate();
-        session.getTransaction().commit();
-        session.close();
-        return itemUpdated > 0;
+        return tx(session -> session.createQuery("update Item i "
+                                + "set i.created = :created, i.name = :name, i.description = :description, i.done = :done "
+                                + "where i.id = :fId")
+                        .setParameter("created", LocalDateTime.now().format(formatter))
+                        .setParameter("name", item.getName())
+                        .setParameter("description", item.getDescription())
+                        .setParameter("done", item.isDone())
+                        .setParameter("fId", id)
+                        .executeUpdate(),
+                sf) > 0;
     }
 
     public boolean updateOnlyState(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        int itemUpdated = session.createQuery("update Item i "
-                        + "set i.done = true "
-                        + "where i.id = :fId")
-                .setParameter("fId", id)
-                .executeUpdate();
-        session.getTransaction().commit();
-        session.close();
-        return itemUpdated > 0;
+        return tx(session -> session.createQuery("update Item i "
+                                + "set i.done = true "
+                                + "where i.id = :fId")
+                        .setParameter("fId", id)
+                        .executeUpdate(),
+                sf) > 0;
     }
 
     public List<Item> findAll() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List<Item> items = session.createQuery("from Item").list();
-        session.getTransaction().commit();
-        session.close();
-        return items;
+        return tx(session -> session.createQuery("from Item").list(), sf);
     }
 
     public Item findById(int id) {
-        Item item;
-        Session session = sf.openSession();
-        session.beginTransaction();
-        item = (Item) session.createQuery("from Item i where i.id = :fId")
-                .setParameter("fId", id)
-                .getSingleResult();
-        session.getTransaction().commit();
-        session.close();
-        return item;
+        return (Item) tx(session -> session.createQuery("from Item i where i.id = :fId")
+                        .setParameter("fId", id)
+                        .getSingleResult(),
+                sf);
     }
 
     public List<Item> findCompletedTasks() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List<Item> items = session.createQuery("from Item i where i.done = true").list();
-        session.getTransaction().commit();
-        session.close();
-        return items;
+        return tx(session -> session.createQuery("from Item i where i.done = true").list(), sf);
     }
 
     public List<Item> findNewTasks() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List<Item> items = session.createQuery("from Item i where i.done = false").list();
-        session.getTransaction().commit();
-        session.close();
-        return items;
+        return tx(session -> session.createQuery("from Item i where i.done = false").list(), sf);
     }
 
     public boolean delete(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        int itemDeleted = session.createQuery("delete from Item i where i.id = :fId")
-                .setParameter("fId", id)
-                .executeUpdate();
-        session.getTransaction().commit();
-        session.close();
-        return itemDeleted > 0;
+        return tx(session -> session.createQuery("delete from Item i where i.id = :fId")
+                        .setParameter("fId", id)
+                        .executeUpdate(),
+                sf) > 0;
     }
 }
